0da6b7fd2a7659f28dcc77035e5789012b988874
iamganesh <iamganeshkanaki@gmail.com>
2024-02-08 10:02:29 +0530

    add bootstrap in react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './Pages/Navbar';
 
 class App extends Component {
-  render() {
+  render() {    
     return (
-      <div>
-        <h1> Ganesh </h1>
+      <div> 
+        <Navbar />     
+        <ul>
+          <li>Home</li>
+          <li>about</li>
+          <li>Contact</li>
+        </ul>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis fugiat nemo dolorem sapiente, aut ex minus voluptatem expedita placeat recusandae iste, officiis eveniet accusamus! Animi facilis illo similique expedita reprehenderit numquam laudantium et beatae.</p>
       </div>
     );
   }
